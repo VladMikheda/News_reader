7298f2573880a5a5700f73b721ebe95591502c02
Vlad Mikheda <vladmikheda@gmail.com>
2022-10-03 02:19:51 +0200

    add a class for parsing arguments, added a method for parsing arguments and a method for checking the validity of arguments

---
+++ ParseArguments.cpp
@@ -0,0 +1,65 @@
+#include "ParseArguments.h"
+
+void ParseArguments::checkArguments(int argc, char** argv){
+
+    for (int i = 1; i < argc; i++){
+        //parse argument to string
+        std::string argument = searchArgument(argv[i]);
+
+        if(argument == CERTIFICATE_FILE){
+            checkArgumentValue(argc, i);
+            certificateFileGiven = true;
+            certificateFilePath = argv[++i]; // path entry for the certificate
+            //todo path check
+
+        }else if(argument == CERTIFICATE_DIRECTORY){
+            checkArgumentValue(argc, i);
+            certificateDirectoryGiven = true;
+            certificateDirectoryPath = argv[++i];
+        }else if(argument == FEED_FILE){
+            if(urlGiven){
+                Error::errorPrint(Error::ERROR_URL_OR_FEED_FILE);
+            }
+            checkArgumentValue(argc,i);
+            feedFileGiven = true;
+            feedFilePath = argv[++i];
+        }else if(argument == TIME_CHANGE){
+            timeChangeGiven = true;
+        }else if(argument == NAME_AUTHOR){
+            nameAuthorGiven = true;
+        }else if(argument == ASSOCIATED_URL){
+            associatedUrlGiven = true;
+        }
+        else{
+            if(feedFileGiven){
+                Error::errorPrint(Error::ERROR_URL_OR_FEED_FILE);
+            }
+            checkArgumentValue(argc,i);
+            urlGiven = true;
+            url = argv[++i];
+        }
+
+    }
+
+    if(!urlGiven && !feedFileGiven){
+        Error::errorPrint(Error::ERROR_URL_OR_FEED_FILE);
+    }
+
+}
+
+std::string ParseArguments::searchArgument(char *arg) {
+    return (arg[0] == '-') ? std::string {&arg[1]} : std::string(arg);
+}
+
+void ParseArguments::checkArgumentValue(int argc, int number) {
+    if(number + 1 >=  argc){
+        Error::errorPrint(Error::ERROR_ARGUMENT_NOT_VALUE);
+    }
+}
+
+void ParseArguments::checkUrl() {
+
+}
+
+
+// (http|https)://[a-zA-Z\.0-9-]+(:[0-9]+.+|[^:]+)
---
+++ ParseArguments.h
@@ -0,0 +1,49 @@
+
+#ifndef ISA_PARSEARGUMENTS_H
+#define ISA_PARSEARGUMENTS_H
+#include "Error.h"
+//using namespace std;
+
+
+class ParseArguments {
+
+private:
+
+    std::string url{0};
+    u_int16_t port; // specify by protocol value ???
+    std::string feedFilePath{0};
+    std::string certificateFilePath{0};
+    std::string certificateDirectoryPath{0};
+    bool timeChangeGiven = false;
+    bool nameAuthorGiven = false;
+    bool associatedUrlGiven = false;
+
+    // bool variables to indicate whether an argument is given
+    bool feedFileGiven = false;
+    bool urlGiven = false;
+    bool portGiven = false;
+    bool certificateFileGiven = false;
+    bool certificateDirectoryGiven = false;
+
+
+    const std::string CERTIFICATE_FILE = "c";
+    const std::string CERTIFICATE_DIRECTORY = "C";
+    const std::string FEED_FILE = "f";
+    const std::string TIME_CHANGE = "T";
+    const std::string NAME_AUTHOR = "a";
+    const std::string ASSOCIATED_URL = "u";
+
+
+public:
+    void checkArguments(int argc, char** argv);
+
+private:
+    std::string searchArgument(char* arg);
+    void checkArgumentValue(int argc, int number);
+    void checkUrl();
+
+};
+
+
+
+#endif //ISA_PARSEARGUMENTS_H
