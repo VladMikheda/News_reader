70af5ce6047afc041f03a9b8610b820bc3ed5ac3
Vlad Mikheda <vladmikheda@gmail.com>
2022-10-20 04:37:20 +0200

    add parse RSS and Atom

--- XMLParser.cpp
+++ XMLParser.cpp
@@ -1,9 +1,9 @@
 
 #include "XMLParser.h"
 
-void XMLParser::parse(const std::string& xmlString) {
-    xmlDoc *doc = nullptr;
-    doc = xmlParseDoc((xmlChar*)xmlString.c_str());
+bool XMLParser::parse(const std::string& xmlString) {
+
+    xmlDoc *doc = xmlParseDoc((xmlChar*)xmlString.c_str());
     if(!doc){
         //todo error
         std::cout << "Error not open XNL string" << std::endl;
@@ -13,31 +13,171 @@ void XMLParser::parse(const std::string& xmlString) {
     if(!rootElement){
         std::cout << "Error not exist root element" << std::endl;
     }
+    checkFormat();
+    if(rss){
+        parseInformationRSS();
+    }else if(atom){
+        parseFeedAtom();
+    }else{
+        return false;
+    }
 
-    std::cout << rootElement->ns->href << std::endl;
-
+    return true;
 }
 
 void XMLParser::checkFormat() {
+        if(!xmlStrcmp(rootElement->name, (const xmlChar*)"rss")){
+            if(!xmlStrcmp(rootElement->properties->children->content, (const xmlChar*)"2.0")){
+                rss = true;
+            }else{
+                //todo error
+                std::cout << "1 not rss / atom" << std::endl;
+            }
+        }else if(!xmlStrcmp(rootElement->name, (const xmlChar*)"feed")){
+            atom = true;
+        }else{
+            //todo error
+            std::cout << "not rss / atom" << std::endl;
+        }
+}
+
+void XMLParser::parseFeedAtom() {
+    for( xmlNode *mainNode = rootElement->children; mainNode; mainNode = mainNode->next){
+
+        if(!xmlStrcmp(mainNode->name, (const xmlChar*)"title")) {
+            //todo call print function
+            std::cout << "****" << xmlNodeGetContent(mainNode) << "****" << std::endl;
+            firstItem = true;
+        }
+
+        if(!xmlStrcmp(mainNode->name, (const xmlChar*)"entry")) {
+            parseRecordsAtom(mainNode);
+        }
+
+
+    }
+
+}
+void XMLParser::parseRecordsAtom(xmlNode* entryNode) {
+    xmlChar *elements[ALL_ITEM_ELEMENT]{nullptr,nullptr,nullptr,nullptr};
+
+    for(xmlNode *itemNode = entryNode->children; itemNode; itemNode = itemNode->next) {
+        if (!xmlStrcmp(itemNode->name, (const xmlChar *) "title")) {
+            elements[TITLE] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+        }
+        else if (!xmlStrcmp(itemNode->name, (const xmlChar *) "link")) {
+            //todo method
+//            elements[LINK] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode->properties->children) << std::endl;
+        }
+//        else if (!xmlStrcmp(itemNode->name, (const xmlChar *) "author")) {
+            //todo method
+//            elements[AUTHOR] = xmlNodeGetContent(itemNode);
+////            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+//        }
+        else if (!xmlStrcmp(itemNode->name, (const xmlChar *) "updated")) {
+            elements[TIME] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+        }
+
+
+    }
+    printRecordInformation(elements);
+}
+
+
+void XMLParser::parseInformationRSS() {
+
+    for( xmlNode *mainNode = rootElement->children; mainNode; mainNode = mainNode->next){
+
+        if(!xmlStrcmp(mainNode->name, (const xmlChar*)"channel")) {
+            parseChannelRSS(mainNode);
+        }
+
+    }
+}
+
+void XMLParser::parseChannelRSS(xmlNode* mainNode ) {
+    for(xmlNode *channelNode = mainNode->children; channelNode; channelNode = channelNode->next){
+
+        if(!xmlStrcmp(channelNode->name, (const xmlChar*)"title")){
+            //todo call print function
+            std::cout << "****" << xmlNodeGetContent(channelNode) << "****" << std::endl;
+            firstItem = true;
+        }
+
+        else if(!xmlStrcmp(channelNode->name, (const xmlChar*)"item")){
+            parseRecordRSS(channelNode);
+        }
+
+    }
+}
+
+
+void XMLParser::parseRecordRSS(xmlNode* channelNode) {
+    xmlChar *elements[ALL_ITEM_ELEMENT]{nullptr,nullptr,nullptr,nullptr};
+
+    for(xmlNode *itemNode = channelNode->children; itemNode; itemNode = itemNode->next) {
+        if (!xmlStrcmp(itemNode->name, (const xmlChar *) "title")) {
+            elements[TITLE] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+        }
+        else if (!xmlStrcmp(itemNode->name, (const xmlChar *) "link")) {
+            elements[LINK] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+        }
+        else if (!xmlStrcmp(itemNode->name, (const xmlChar *) "author")) {
+            elements[AUTHOR] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+        }
+        else if (!xmlStrcmp(itemNode->name, (const xmlChar *) "pubDate")) {
+            elements[TIME] = xmlNodeGetContent(itemNode);
+//            std::cout << xmlNodeGetContent(itemNode) << std::endl;
+        }
+
+
+    }
+    printRecordInformation(elements);
+}
+
+void XMLParser::printRecordInformation(xmlChar **elements) {
+
+    for(int i = 0; i < ALL_ITEM_ELEMENT; i++){
+        if(elements[i]){
+            if(i == TITLE){
+                if(!firstItem){
+                    std::cout << "\n";
+                }
+            }
+            if(i == LINK && !url || i == AUTHOR && !author || i == TIME && !time){
+                continue;
+            }
+
+            std::cout << elements[i] << std::endl;
+        }else{
+            if(i == 0 && ((url && elements[LINK])||(author && elements[AUTHOR])||(time && elements[TIME]) )){
+                if(!firstItem){
+                    std::cout << "\n";
+                }
+                std::cout << "----" << std::endl;
+            }
+        }
+    }
+    firstItem = false;
 
 }
 
-void XMLParser::parseInformation() {
-
+XMLParser::XMLParser(bool url, bool author, bool time) {
+    this->url = url;
+    this->author = author;
+    this->time = time;
 }
 
-void XMLParser::parseMainInformationAtom() {
 
-}
+//    xmlNodeGetContent(rootElement->children->next->children)
+//    std::cout <<rootElement->children->next->children->next->name << std::endl;
+//    std::cout << xmlNodeGetContent(rootElement->children->next->children->next )<< std::endl;
 
-void XMLParser::parseRecordsAtom() {
 
-}
 
-void XMLParser::parseMainInformationRSS() {
-
-}
-
-void XMLParser::parseRecordRSS() {
-
-}
--- XMLParser.h
+++ XMLParser.h
@@ -5,23 +5,39 @@
 #include <libxml/parser.h>
 #include <string>
 
-
+#define ALL_ITEM_ELEMENT 4
 class XMLParser {
 
 private:
+
+    const u_int8_t TITLE = 0;
+    const u_int8_t LINK = 1;
+    const u_int8_t AUTHOR = 2;
+    const u_int8_t TIME = 3;
+
+
     bool atom = false;
     bool rss  = false;
+    bool time = false;
+    bool url = false;
+    bool author = false;
+
+    bool firstItem = false;
+
     xmlNode *rootElement = nullptr;
 
     void checkFormat();
-    void parseInformation();
-    void parseMainInformationAtom();
-    void parseRecordsAtom();
-    void parseMainInformationRSS();
-    void parseRecordRSS();
-public:
+    void parseFeedAtom();
+    void parseRecordsAtom(xmlNode* entryNode );
+    void parseInformationRSS();
+    void parseChannelRSS(xmlNode* mainNode);
+    void parseRecordRSS(xmlNode* channelNode);
+    void printMainTitle();
+    void printRecordInformation(xmlChar **elements);
 
-    void parse(const std::string& xmlString);
+public:
+    XMLParser(bool url, bool author, bool time);
+    bool parse(const std::string& xmlString);
 };
 
 
