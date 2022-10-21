bcfd70c4a9e3221b14e75e9cbf7cf468c660918e
Vlad Mikheda <vladmikheda@gmail.com>
2022-10-21 03:56:17 +0200

    add comments

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
+/**
+ * Project: Čtečka novinek ve formátu Atom a RSS s podporou TLS
+ *
+ * File:     main.cpp
+ * Subject:  ISA 2022
+ *
+ * @author:  Vladislav Mikheda  xmikhe00
+ */
 
-#include <iostream>
-#include <unistd.h>
-#include <fstream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include "openssl/bio.h"
-
-#include <netdb.h>
-#include <arpa/inet.h>
-//using namespace std;
 
 #include "FeedReader.h"
 
-# include  "openssl/bio.h"
-# include  "openssl/ssl.h"
-# include  "openssl/err.h"
-
+/**
+ * The main function of the program,
+ * entrance to the program
+ * creates an instance of the FeedReader class and launches their main functions
+ */
 int main(int argc, char** argv) {
 
     FeedReader feedReader;
     feedReader.read(argc,argv);
 
-
+    return 0;
 //    //initialization
 //    SSL_library_init();
 //    SSL_load_error_strings();
