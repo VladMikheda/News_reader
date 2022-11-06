cb006b731301abdc67c7f9eb6190f9a7638c66b5
Vlad Mikheda <vladmikheda@gmail.com>
2022-11-06 03:17:52 +0100

    add injection file

---
+++ tests/Error.cpp
@@ -0,0 +1,18 @@
+
+#include "../Error.h"
+
+void Error::errorPrint(int errorNumber) {
+    switch (errorNumber) {
+        default:
+            break;
+    }
+}
+
+void Error::printMessage(std::string message) {
+
+}
+
+void Error::exitProgram(int exitNumber) {
+
+}
+
