f84804f8587d60f74e222094cee994b21c2c984e
Vlad Mikheda <vladmikheda@gmail.com>
2022-11-06 03:20:05 +0100

    new test class

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -1,36 +1,73 @@
 #include <cassert>
 #include "UnitTests.h"
 #include "TestArgumentParser.h"
+#include "TestUrlParser.h"
+#include "TestConnect.h"
+#include "TestXMLParser.h"
 
 void UnitTests::testArgumentParser() {
-    TestArgumentParser testArgumentParser;
+    TestArgumentParser testArgumentsParser;
 
-    testArgumentParser.emptyArguments();
-    testArgumentParser.URLArgument();
-    testArgumentParser.feedFileNoPathArgument();
-    testArgumentParser.feedFileArgument();
-    testArgumentParser.urlAndFeedFileArgument();
-    testArgumentParser.certificateDirNoPathArgument();
-    testArgumentParser.certificateDirArgument();
-    testArgumentParser.certificateFileNoPathArgument();
-    testArgumentParser.certificateFileArgument();
-    testArgumentParser.certificateDirAndFileArgument();
-    testArgumentParser.printTimeArgument();
-    testArgumentParser.printAuthorArgument();
-    testArgumentParser.printUrlArgument();
-    testArgumentParser.allNormalArguments();
+    testArgumentsParser.emptyArguments();
+    testArgumentsParser.URLArgument();
+    testArgumentsParser.feedFileNoPathArgument();
+    testArgumentsParser.feedFileArgument();
+    testArgumentsParser.urlAndFeedFileArgument();
+    testArgumentsParser.certificateDirNoPathArgument();
+    testArgumentsParser.certificateDirArgument();
+    testArgumentsParser.certificateFileNoPathArgument();
+    testArgumentsParser.certificateFileArgument();
+    testArgumentsParser.certificateDirAndFileArgument();
+    testArgumentsParser.printTimeArgument();
+    testArgumentsParser.printAuthorArgument();
+    testArgumentsParser.printUrlArgument();
+    testArgumentsParser.allNormalArguments();
 
     std::cout << "TEST:Tests ArgumentParser are SUCCESS" << std::endl;
 }
 
 void UnitTests::testURLParser() {
+    TestUrlParser testUrlParser;
 
+    testUrlParser.emptyUrl();
+    testUrlParser.emptyPortUrl();
+    testUrlParser.httpUrl();
+    testUrlParser.httpsUrl();
+    testUrlParser.portUrl();
+    testUrlParser.raveUrl();
+    testUrlParser.wrongSchemeUrl();
+    testUrlParser.wrongSlashUrl();
+    testUrlParser.emptyPortUrl();
+    testUrlParser.wrongPortNumberUrl();
+    testUrlParser.wrongPortLetterUrl();
+    testUrlParser.allUrl();
+
+    std::cout << "TEST:Tests URLParser are SUCCESS" << std::endl;
 }
 
 void UnitTests::testConnect() {
+    TestConnect testConnects;
 
+    testConnects.connectHTTP();
+    testConnects.connectDefaultHTTPS();
+    testConnects.connectCertDirHTTPS();
+    testConnects.connectCertFileHTTPS();
+    testConnects.connectCertFileAndDirHTTPS();
+    testConnects.twoConnect();
+
+    std::cout << "TEST:Tests Connect are SUCCESS" << std::endl;
 }
 
 void UnitTests::XMLParser() {
+    TestXMLParser testXmlParser;
+    testXmlParser.RSSNormal();
+}
+
+void UnitTests::testAll() {
+//    testArgumentParser();
+//    testURLParser();
+//    testConnect();
+    XMLParser();
+
 
 }
--- tests/UnitTests.h
+++ tests/UnitTests.h
@@ -15,37 +15,13 @@
 //#define maxString 1000
 
 class UnitTests {
-private:
-    std::string programName = "program name";
-    std::string urlSting = "https://www.theregister.co.uk/headlines.atom";
-//    char *argumentFeedList = "-f";
-//    char *argumentCertificateFile = "-c";
-//    char *argumentCertificatePath = "-C";
-//    char *argumentTime = "-T";
-//    char *argumentAuthor = "-a";
-//    char *argumentURL = "-u";
 
-//    const char arguments1[countArguments1][maxString] = {"program name"};
-//    const char arguments2[countArguments2][maxString] = {"program name","https://en.wikipedia.org/w/api.php?hidebots=1&days=7&limit=50&hidewikidata=1&action=feedrecentchanges&feedformat=atom"};
-//    const char arguments3[countArguments3][maxString] = {"program name","https://en.wikipedia.org:4333/w/api.php?hidebots=1&days=7&limit=50&hidewikidata=1&action=feedrecentchanges&feedformat=atom"};
-//    const char arguments4[countArguments4][maxString] = {"program name","-f","/home/goofy/study/ISA/ISA/tests/feedfiletest"};
-//    const char arguments5[countArguments5][maxString] = {"program name","-f"};
-//    const char arguments6[countArguments6][maxString] = {"program name","https://en.wikipedia.org/w/api.php?hidebots=1&days=7&limit=50&hidewikidata=1&action=feedrecentchanges&feedformat=atom","-f","/home/goofy/study/ISA/ISA/tests/feedfiletest"};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-C",};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-c"};
-//    const char arguments7[countArguments1][maxString] = {"program name","-C"};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-C","/etc/ssl/certs"};
-//    const char arguments8[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-c","SecureTrust_CA.pem"};
-//    const char arguments9[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-C","/etc/ssl/certs","-c","SecureTrust_CA.pem"};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-T"};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-a"};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-u"};
-//    const char arguments7[countArguments1][maxString] = {"program name","https://www.theregister.co.uk/headlines.atom","-C","/etc/ssl/certs","-c","SecureTrust_CA.pem","-T","-a","-u"};
 public:
     void testArgumentParser();
     void testURLParser();
     void testConnect();
     void XMLParser();
+    void testAll();
 };
 
 
